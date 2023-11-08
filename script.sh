5b8674e2f6fe52f22557f4f18582c8b44c842992
iiakhtar <147703109+iiakhtar@users.noreply.github.com>
2023-11-08 18:58:21 +0530

    Add files via upload

--- script.sh
+++ script.sh
@@ -2,4 +2,5 @@
 mkdir cloneRepos
 cd cloneRepos
 git clone https://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@github.com/<user>/iiakhtar/automation-aws
+echo $HELLO
 robot --outputdir tmp /About.robot
