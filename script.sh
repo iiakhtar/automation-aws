02322f2b75c20094f87f34ecb4eaf42cdd5676e7
iiakhtar <147703109+iiakhtar@users.noreply.github.com>
2023-11-08 19:32:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 mkdir cloneRepos
 cd cloneRepos
-git clone https://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@github.com/<user>/iiakhtar/automation-aws
+git clone https://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@github.com/iiakhtar/automation-aws
 echo $HELLO
-robot --outputdir tmp /About.robot
+robot --outputdir tmp ./automation-aws/About.robot
