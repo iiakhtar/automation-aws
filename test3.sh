15ffbae180a07c6fe460f3bd87b10b4878faa2d4
iiakhtar <147703109+iiakhtar@users.noreply.github.com>
2023-11-09 12:49:18 +0530

    Delete test3.sh

--- test3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir cloneRepos
-cd cloneRepos
-echo "in script file"
-git clone https://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@github.com/iiakhtar/automation-aws
-echo $HELLO
-ls
-robot --outputdir /tmp automation-aws/About.robot
